3fadb88e3e9b8fa7901b138d5fa19ef82ac29790
Christian Ide <cide@gmx.de>
2024-12-15 15:10:05 +0100

    Moved test input

--- src/test/java/aoc/day01/Day01Test.java
+++ src/test/java/aoc/day01/Day01Test.java
@@ -1,47 +1,30 @@
 package aoc.day01;
 
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class Day01Test {
 
+    private final static String input = """
+            3   4
+            4   3
+            2   5
+            1   3
+            3   9
+            3   3
+            """;
+
     @Test
-    public void testPart1(){
-        // Given
-        final String input = """
-                3   4
-                4   3
-                2   5
-                1   3
-                3   9
-                3   3
-                """;
-
-        // When
+    public void testPart1() {
         final String result = new Day01().part1(input);
-
         final String expected = "11";
-        // Then
         Assertions.assertEquals(expected, result);
     }
 
     @Test
-    public void testPart2(){
-        // Given
-        final String input = """
-                3   4
-                4   3
-                2   5
-                1   3
-                3   9
-                3   3
-                """;
-        // When
+    public void testPart2() {
         final String result = new Day01().part2(input);
-
         final String expected = "31";
-        // Then
         Assertions.assertEquals(expected, result);
     }
 }
